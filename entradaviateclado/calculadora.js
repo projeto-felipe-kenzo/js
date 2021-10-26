fd684b23069f2e88be43b0cb44105c53ec18c115
projeto-felipe-kenzo <79851474+projeto-felipe-kenzo@users.noreply.github.com>
2021-10-26 09:34:46 -0300

    Add files via upload

---
+++ entradaviateclado/calculadora.js
@@ -0,0 +1,20 @@
+var num1 = parseInt(prompt('Digite o num1'));
+var num2 = parseInt(prompt('Digite o num2'));
+
+var soma = num1+num2;
+var subtração = num1-num2;
+var multiplicação = num1*num2;
+var divisão = num1/num2;
+
+document.getElementById('num1'). innerHTML = num1;
+document.getElementById('num2'). innerHTML = num2;
+document.getElementById('soma'). innerHTML = soma;
+document.getElementById('subtração'). innerHTML = subtração;
+document.getElementById('multiplicação'). innerHTML = multiplicação;
+document.getElementById('divisão'). innerHTML = divisão;
+
+
+//console.log(soma);
+//console.log(subtração);
+//console.log(multiplicação);
+//console.log(divisão);
---
+++ variáveis/exemplo.js
@@ -0,0 +1,10 @@
+var texto = "Vamos fazer uma calculadora"
+var num1 = 25;
+var num2 = 26;
+soma = num1+num2
+alert(soma);
+
+//document.getElementById('teste').innerHTML = "AAAAAAAAAAAAAA";
+console.log(num1);
+
+document.getElementById('teste').innerHTML = texto;
